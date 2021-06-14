027ee19914f12dc98414a61d163aa0dfa11aa79a
tsabz <tonia.saba@gmail.com>
2021-06-14 19:32:40 -0400

    renamed variables

---
+++ module_6.py
@@ -0,0 +1,12 @@
+
+user_input = "helo"
+
+
+def is_recursion(user_input):
+    recurser_array = []
+    for letters in user_input:
+        recurser_array.append(letters)
+    print(recurser_array)
+
+
+is_recursion(user_input)
